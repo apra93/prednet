8f9233462fd86fbc9b0a5010ed4f0749b9159a56
apra <dd.apra93@gmail.com>
2020-08-28 15:04:34 -0400

    Changing to pathlib paths

--- prednet/kitti_settings.py
+++ prednet/kitti_settings.py
@@ -1,10 +1,14 @@
+from pathlib import Path
+REPO_DIR = Path(__file__).resolve().parent.parent
+PN_DIR = REPO_DIR / 'prednet'
+
 # Where KITTI data will be saved if you run process_kitti.py
 # If you directly download the processed data, change to the path of the data.
-DATA_DIR = './kitti_data/'
+DATA_DIR = PN_DIR / 'kitti_data/'
 
 # Where model weights and config will be saved if you run kitti_train.py
 # If you directly download the trained weights, change to appropriate path.
-WEIGHTS_DIR = './model_data_keras2/'
+WEIGHTS_DIR = PN_DIR / 'model_data_keras2/'
 
 # Where results (prediction plots and evaluation file) will be saved.
-RESULTS_SAVE_DIR = './kitti_results/'
+RESULTS_SAVE_DIR = PN_DIR / 'kitti_results/'
